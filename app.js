fe98cac5622727771ef8455bbcb0cbd0d3f17779
felprangel <feliperangel897@gmail.com>
2024-08-24 19:08:00 -0300

    adiciona configurações para as views

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const indexRouter = require("./routes/indexRouter");
 const messageRouter = require("./routes/newMessageRouter");
+const path = require("node:path");
 const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.use("/", indexRouter);
 app.use("/new", messageRouter);
