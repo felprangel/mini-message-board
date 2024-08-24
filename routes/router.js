23ae694e51134527ec5ddbfe54213784150588af
felprangel <feliperangel897@gmail.com>
2024-08-24 19:24:24 -0300

    adiciona lógica de nova mensagem

--- routes/router.js
+++ routes/router.js
@@ -23,6 +23,13 @@ router.get("/new", (req, res) => {
   res.render("form");
 });
 
-router.post("/new", (req, res) => {});
+router.post("/new", (req, res) => {
+  messages.push({
+    text: req.body.message,
+    user: req.body.user,
+    added: new Date(),
+  });
+  res.redirect("/");
+});
 
 module.exports = router;
