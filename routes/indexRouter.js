407ee69b252e2a52ace39757dd6cae2a3b3a5890
felprangel <feliperangel897@gmail.com>
2024-08-24 19:02:19 -0300

    adiciona array de mensagens

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,6 +2,19 @@ const { Router } = require("express");
 
 const indexRouter = Router();
 
+const messages = [
+  {
+    text: "Olá!",
+    user: "Rafael",
+    added: new Date(),
+  },
+  {
+    text: "Olá mundo!",
+    user: "Bernardo",
+    added: new Date(),
+  },
+];
+
 indexRouter.get("/", (req, res) => {
   res.send("Hello World!");
 });
