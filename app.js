59e525e27126333dbbfcca5ffbaf868f49f89020
felprangel <feliperangel897@gmail.com>
2024-08-24 19:22:57 -0300

    adiciona importação

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = require("./routes/router");
+const path = require("node:path");
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
