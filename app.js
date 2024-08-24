e4309757ef14aede850689680f94329537174840
felprangel <feliperangel897@gmail.com>
2024-08-24 19:20:48 -0300

    remove messageRouter

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const indexRouter = require("./routes/indexRouter");
-const messageRouter = require("./routes/newMessageRouter");
-const path = require("node:path");
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
@@ -10,7 +8,6 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use("/", indexRouter);
-app.use("/new", messageRouter);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
