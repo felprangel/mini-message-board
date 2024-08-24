814379c22ba1c983f4413f71208e2fa721a4e67f
felprangel <feliperangel897@gmail.com>
2024-08-24 18:55:25 -0300

    adiciona rota de nova mensagem

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+app.get("/new", (req, res) => {
+  res.send("Hello World");
+});
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
