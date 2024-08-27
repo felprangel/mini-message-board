e2fc73b8c32b1c87b188bd9c811b4f25a4ef15d9
felprangel <feliperangel897@gmail.com>
2024-08-27 20:15:17 -0300

    adiciona porta em uma variável

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const router = require("./routes/router");
 const path = require("node:path");
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 app.use(express.urlencoded({ extended: true }));
 
 app.set("views", path.join(__dirname, "views"));
@@ -10,6 +12,6 @@ app.set("view engine", "ejs");
 
 app.use("/", router);
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
