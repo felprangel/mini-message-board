7ea8e70f7e8643b15889d975498beb3c0e3baaec
felprangel <feliperangel897@gmail.com>
2024-08-31 18:27:29 -0300

    consulta mensagens

--- routes/router.js
+++ routes/router.js
@@ -3,6 +3,7 @@ const { Router } = require("express");
 const router = Router();
 
 router.get("/", (req, res) => {
+  const mensagens = await db.getMessages();
   res.render("index", { title: "Mini Messageboard", messages });
 });
 
