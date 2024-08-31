f17ea620c62ce397840aa53fa251aea8e4c10ecb
felprangel <feliperangel897@gmail.com>
2024-08-31 18:26:54 -0300

    adiciona arquivos para queries

---
+++ db/queries.js
@@ -0,0 +1,8 @@
+const pool = require("./pool");
+
+async function getMessages() {
+  const { rows } = await pool.query("SELECT * FROM messages");
+  return rows;
+}
+
+module.exports = { getMessages };
