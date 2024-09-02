16e4157f1435beaf7d90f6b2e750bf13dd7faa0f
felprangel <feliperangel897@gmail.com>
2024-09-02 20:37:12 -0300

    formata data e retorna o primeiro resultado

--- db/queries.js
+++ db/queries.js
@@ -9,7 +9,8 @@ async function getMessage(id) {
   const { rows } = await pool.query("SELECT * FROM messages WHERE id = $1", [
     id,
   ]);
-  return rows;
+  rows[0].data_criacao = new Date(rows[0].data_criacao).toLocaleString("pt-BR");
+  return rows[0];
 }
 
 async function createMessage(message, user) {
